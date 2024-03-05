5c1afcc1ea5fdce01b1296df404b93661a151a5c
Allen <39230030+Kyriection@users.noreply.github.com>
2024-03-04 21:49:14 -0600

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,6 @@
-nohup bash src/ms_poe.sh 1 2 > log_ours_1.out 2>&1 &
-nohup bash src/ms_poe.sh 3 3 > log_ours_3.out 2>&1 &
+# nohup bash src/ms_poe.sh 1 2 > log_ours_1.out 2>&1 &
+# nohup bash src/ms_poe.sh 3 3 > log_ours_3.out 2>&1 &
+
+nohup bash src/ms_poe.sh 5 0 > log_ours_5.out 2>&1 &
+nohup bash src/ms_poe.sh 7 3 > log_ours_7.out 2>&1 &
+nohup bash src/ms_poe.sh 10 3 > log_ours_10.out 2>&1 &
